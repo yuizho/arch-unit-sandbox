75e2c705c37ee75e304bac0fdfd035f7589b6e74
yuizho <yui-ito@bm-sms.co.jp>
2022-12-03 13:37:06 +0900

    modify ArchUnitTest.java to add comments

--- src/test/java/com/github/yuizho/ArchUnitTest.java
+++ src/test/java/com/github/yuizho/ArchUnitTest.java
@@ -8,6 +8,8 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packages = "com.github.yuizho")
 public class ArchUnitTest {
+    // jsut shakyou code
+    // https://www.archunit.org/userguide/html/000_Index.html#_using_junit_4_or_junit_5
     @ArchTest
     public static final ArchRule myRule = classes()
             .that().resideInAnyPackage("..service..")
